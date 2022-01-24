65332422ae3efddb316bca578ef12c68abfdac05
ada2030 <osorovadahan04@gmail.com>
2022-01-24 23:41:46 +0300

    Верстка шаблона таба.

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,7 @@ var sam = new function() {
 
     self.init = function() {
         self.menu.init();
+        self.tabs.init();
     };
 
     this.menu = new function() {
@@ -35,5 +36,82 @@ var sam = new function() {
                 $(submenu).toggleClass('open');
             });
         };
+    };
+    this.tabs = new function() {
+        let that = this;
+
+        this.init = function() {
+            $('.js-tabs:not(.js-already-init)').each(function() {
+                that.build(this);
+            });
+        };
+
+        this.customize = {
+            li: function(li) {
+                if ($(li).hasClass('js-active')) {
+                    $(li).addClass('tabs__item--active');
+                } else {
+                    $(li).removeClass('tabs__item--active');
+                }
+            },
+            tab: function(tab) {
+                if ($(tab).hasClass('js-show')) {
+                    $(tab).addClass('tabs__pane--active');
+                } else {
+                    $(tab).removeClass('tabs__pane--active');
+                }
+            }
+        };
+
+        this.build = function(ul) {
+            if (!$(ul).data('cont-id') && !$($(ul).data('cont-selector'))) {
+                return false;
+            }
+
+            let tabsCont;
+            if ($(ul).data('cont-id')) {
+                tabsCont = $('#' + $(ul).data('cont-id'));
+            } else {
+                tabsCont = $($(ul).data('cont-selector'));
+            }
+
+            $(ul).find('li a').click(function() {
+                let li = $(this).parents('li');
+                $(ul).find('li.js-active').removeClass('js-active');
+                $(li).addClass('js-active');
+                $(ul).find('li').each(function() {
+                    that.customize.li(this);
+                });
+
+                let tabContSelector;
+
+                if ($(li).data('tab-id')) {
+                    tabContSelector = '>.js-tab-cont#' + $(li).data('tab-id');
+                } else if ($(ul).data('tab-selector')) {
+                    tabContSelector = '>.js-tab-cont' + $(li).data('tab-selector');
+                }
+
+                let tab = $(tabsCont).find(tabContSelector);
+                $(tabsCont).find('>.js-tab-cont.js-show').removeClass('js-show').hide();
+                $(tab).addClass('js-show').show();
+                $.each($(tabsCont).find('>.js-tab-cont'), function() {
+                    that.customize.tab(this);
+                });
+
+                let tabsCallback = $(ul).data('callback');
+                if (tabsCallback && that.callbacks[tabsCallback]) {
+                    that.callbacks[tabsCallback](ul);
+                }
+
+            });
+
+            $(ul).addClass('js-already-init');
+        };
+
+        this.destroy = function(ul) {
+            $(ul).find('li a').off('click');
+        };
+
+        this.callbacks = {};
     };
 };
