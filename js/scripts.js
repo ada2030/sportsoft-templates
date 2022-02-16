6563ce14aa2cee1ff24ba59bbb356ad4b122af6c
ada2030 <osorovadahan04@gmail.com>
2022-02-16 17:34:55 +0300

    Правки бургера.

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,7 @@ var sam = new function() {
         self.tabs.init();
         self.tabsMobile.init();
         self.printTable.init();
+        self.tinySlider.init();
     };
 
     this.menu = new function() {
@@ -26,6 +27,11 @@ var sam = new function() {
             $(hamburger).click(function() {
                 $(hamburgerOpen).toggleClass('open');
                 $(this).toggleClass('open');
+                if ($(this).hasClass('open')) {
+                    $('body').addClass('fixed');
+                } else {
+                    $('body').removeClass('fixed');
+                }
             });
 
             $(hasSubmenu).click(function () {
@@ -156,5 +162,116 @@ var sam = new function() {
                 });
             });
         }
+    };
+    this.tinySlider = new function () {
+        let that = this;
+
+        this.sliders = {};
+
+        this.timers = {};
+
+        this.init = function () {
+            $('.js-tiny-slider').each(function (k) {
+                let slider = this, data = $(this).data,
+                    initialWidth = data['initialWidth'], bodyWidth = self.body.outerWidth(),
+                    sliderName = data['sliderName'] ? data['sliderName'] : k;
+                if (initialWidth) {
+                    if (initialWidth >= bodyWidth) {
+                        that.build(this, k);
+                    }
+
+                    $(window).resize(function () {
+                        if (that.timers[sliderName]) {
+                            clearTimeout(that.timers[sliderName]);
+                            delete that.timers[sliderName];
+                        }
+                        that.timers[sliderName] = setTimeout(function () {
+                            let bodyWidth = self.body.outerWidth();
+                            if (initialWidth >= bodyWidth) {
+                                that.build(slider, sliderName);
+                            } else if (that.sliders[sliderName]) {
+                                that.destroy(slider, sliderName);
+                            }
+                        }, 100);
+                    });
+                } else {
+                    that.build(this, sliderName);
+                }
+            });
+        };
+
+        this.build = function (slider, k) {
+            if (!$(slider).hasClass('js-already-init')) {
+                let sliderClass = 'js-tiny-slider-' + k,
+                    sliderSelector = '.' + sliderClass;
+                $(slider).addClass(sliderClass).data('sliderIndex', k);
+                let data = $(slider).data();
+                let params = {
+                    //Контейнеры
+                    container: data['container'] ? data['container'] : sliderSelector, //селектор контейнера для слайдера
+                    controlsContainer: data['controlsContainer'] ? data['controlsContainer'] : false, //селектор контейнера для стрелок
+                    navContainer: data['navContainer'] ? data['navContainer'] : false, //селектор контейнера для точек
+                    //Стрелки и точки
+                    controls: data['controls'] ? data['controls'] : false, //кнопки
+                    prevButton: data['prevButton'] ? $(data['prevButton']).get(0) : false, //селектор кнопки пред. слайда
+                    nextButton: data['nextButton'] ? $(data['nextButton']).get(0) : false, //селектор кнопки след. слайда
+                    nav: data['nav'] ? data['nav'] : false, //точки [dots]
+                    navAsThumbnails: data['navAsThumbnails'] ? data['navAsThumbnails'] : false, //навигация в виде мини-картинок
+
+                    //Основные параметры
+                    mode: data['mode'] ? data['mode'] : 'carousel',
+                    items: data['items'] ? data['items'] : 1,            //количество видимых элементов слайдов
+                    slideBy: data['slideBy'] ? data['slideBy'] : 1,            //на сколько слайдов сдвигать
+                    startIndex: data['startIndex'] ? data['startIndex'] : false,        //начальный слайд
+                    autoWidth: data['autoWidth'] ? data['autoWidth'] : false,        //автоматическое определение ширины слайда
+                    autoHeight: data['autoHeight'] ? data['autoHeight'] : false,        //автоматическое определение высоты слайда,
+                    fixedWidth: data['fixedWidth'] ? data['fixedWidth'] : false,        //фиксированная ширина слайда
+                    loop: data['loop'] ? data['loop'] : false,        //бесконечность прокрутки
+                    speed: data['speed'] ? data['speed'] : 300,          //скорость прокрутки
+                    lazyload: data['lazyload'] ? data['lazyload'] : false,        //ленивая загрузка
+                    axis: data['axis'] ? data['axis'] : 'horizontal', //['horizontal', 'vertical'] горизонтальная/вертикальная прокрутка
+                    gutter: data['gutter'] ? data['gutter'] : 0,            //расстояние между слайдами, в px
+                    center: data['center'] ? data['center'] : false,        //центрирование активного слайда
+                    mouseDrag: data['mouseDrag'] ? data['center'] : false,        //изменение слайдов путем их перетаскивания
+                    touch: data['mouseDrag'] ? data['center'] : false,        //активирует обнаружение ввода для сенсорных устройств.
+
+                    //Автопрокрутка
+                    autoplay: data['autoplay'] ? data['autoplay'] : false,     //автопрокрутка
+                    autoplayButtonOutput: data['autoplayButtonOutput'] ? data['autoplayButtonOutput'] : false,     //кнопки для автопрокрутки
+                    autoplayTimeout: data['autoplayTimeout'] ? data['autoplayTimeout'] : 5000,      //задержка прокрутки
+                    autoplayDirection: data['autoplayDirection'] ? data['autoplayDirection'] : 'forward', //['forward', 'backward'] направленность прокрутки
+                    autoplayText: data['autoplayText'] ? data['autoplayText'] : false,     //['start', 'stop'] //текст кнопок прокрутки
+                    autoplayHoverPause: data['autoplayHoverPause'] ? data['autoplayHoverPause'] : false,     //остановка при наведении мыши
+
+                    //Респонсив
+                    responsive: data['responsive'] ? data['responsive'] : false, //{breakpoint: {key: value, [...]}}}
+
+                    //Отключение hideNav (расширение by @bpystep)
+                    hideNav: data['hide-nav'] !== 'undefined' ? data['hide-nav'] : true //скрывает Nav, если все слайды отображены
+                };
+
+                let tnsSlider = tns(params);
+                this.sliders[data['sliderName'] ? data['sliderName'] : k] = tnsSlider;
+                $(slider).removeClass(sliderClass);
+
+                if ($(slider).data('transitionStart') && that.callbacks[$(slider).data('transitionStart')]) {
+                    tnsSlider.events.on('transitionStart', that.callbacks[$(slider).data('transitionStart')]);
+                }
+
+                if ($(slider).data('transitionEnd') && that.callbacks[$(slider).data('transitionEnd')]) {
+                    tnsSlider.events.on('transitionEnd', that.callbacks[$(slider).data('transitionEnd')]);
+                }
+                $(slider).removeClass(sliderClass);
+                $(slider).addClass('js-already-init');
+            }
+        };
+
+        this.destroy = function (slider, k) {
+            $(slider).removeClass('js-already-init');
+            that.sliders[k].destroy();
+            delete that.sliders[k];
+        };
+
+        this.callbacks = {};
     };
 };
