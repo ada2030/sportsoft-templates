7ac6451bc60250f3b8bfcfd98b231ac66185440a
adakhannn <osorovadahan04@gmail.com>
2023-05-04 17:31:05 +0600

    Доработки календаря

--- js/book.js
+++ js/book.js
@@ -1,51 +1,64 @@
-todoMain();
+bookMain();
 
-function todoMain() {
-  const DEFAULT_OPTION = "Выберите категорию";
+function bookMain() {
+  const CLIENT =  {
+    id:  _uuid(),
+    name: "Имя клиента 1"
+  }
+  const CLIENTS =  [{
+    id:  _uuid(),
+    name: "Имя клиента 1"
+  },
+  {
+    id:  _uuid(),
+    name: "Имя клиента 1"
+  }]
+  const COST = 1500;
+  const REFEREE = "Имя Судьи";
   const holidays = [moment('2023-05-04','YYYY-MM-DD'), moment('2023-04-05','YYYY-MM-DD'), moment('2023-05-07','YYYY-MM-DD'), moment('2023-05-11','YYYY-MM-DD')];
-  
-  let eventName,
-    eventCategory,
-    dateStartInput,
-    dateEndInput,
-    addButton,
-    selectElem,
-    todoList = [],
-    calendar,
-    calendarForm,
-    formClose;
+
+  let placeName,
+      fieldName,
+      dateStartInput,
+      dateEndInput,
+      bookType,
+      amountPaid,
+      addButton,
+      bookList = [],
+      calendar,
+      calendarForm,
+      formClose;
   
   getElements();
   addListeners();
   initCalendar();
   load();
   renderRows();
-  updateSelectOptions();
   
   function getElements() {
-    eventName = document.getElementById("eventName");
-    eventCategory = document.getElementById("eventCategory");
+    placeName = document.getElementById("placeName");
+    fieldName = document.getElementById("fieldName");
     dateStartInput = document.getElementById("dateStartInput");
     dateEndInput = document.getElementById("dateEndInput");
+    bookType = document.getElementById("bookType");
     addButton = document.getElementById("addBtn");
-    selectElem = document.getElementById("categoryFilter");
     calendarForm = document.getElementById("form");
     formClose = document.getElementById("form-close");
+    amountPaid = document.getElementById("amountPaid");
   }
   
   function addListeners() {
     addButton.addEventListener("click", addEntry, false);
-    selectElem.addEventListener("change", filterEntries, false);
   }
   
   function addEntry(event) {
     event.preventDefault();
     
-    let eventNameValue = eventName.value;
-    eventName.value = "";
+    let placeNameValue = placeName.value;
+    placeName.value = "";
     
-    let categoryValue = eventCategory.value;
-    eventCategory.value = "";
+    let fieldNameValue = fieldName.value;
+    fieldName.value = "";
     
     let dateStartValue = dateStartInput.value;
     dateStartInput.value = "";
@@ -53,145 +66,120 @@ function todoMain() {
     let dateEndValue = dateEndInput.value;
     dateEndInput.value = "";
 
+    let bookTypeValue = bookType.value;
+    bookType.value = "";
+
+    console.log(amountPaid.value);
+
+    let amountPaidValue = amountPaid.value;
+    amountPaid.value = "";
+
     let color;
 
-    if (categoryValue === 'Поле 1') {
+    if (fieldNameValue === 'Поле 1') {
       color = "#000000";
-    } else if (categoryValue === 'Поле 2') {
+    } else if (fieldNameValue === 'Поле 2') {
       color = "#ff0000";
+    } else if (fieldNameValue === '1/2 Поля 1') {
+      color = "#6ca992";
+    } else if (fieldNameValue === '1/2 Поля 2') {
+      color = "#c39797";
     }
     
     let obj = {
       id: _uuid(),
-      todo: eventNameValue,
-      category: categoryValue,
+      place: placeNameValue,
+      field: fieldNameValue,
       dateStart: dateStartValue,
       dateEnd: dateEndValue,
+      client: (bookTypeValue === "Чемпионат") ? CLIENTS.map(elem => elem.name).join(",") : CLIENT.name,
+      amountPaid: amountPaidValue,
+      bookType: bookTypeValue,
+      referee: (bookTypeValue === "Чемпионат") ? REFEREE : 'Нет судьи',
       color: color
     };
     
-    rendowRow(obj);
+    renderRow(obj);
     
-    todoList.push(obj);
+    bookList.push(obj);
     
     save();
-    
-    updateSelectOptions();
-  }
-  
-  function filterEntries() {
-    let selection = selectElem.value;
-    
-    let trElems = document.getElementsByTagName("tr");
-    for(let i = trElems.length - 1; i > 0; i--){
-      trElems[i].remove();
-    }
-  
-    calendar.getEvents().forEach(event=>event.remove());
-    
-    if (selection == DEFAULT_OPTION) {
-      todoList.forEach( obj => rendowRow(obj) );
-    } else {
-      todoList.forEach( obj => {
-        if( obj.category == selection ){
-          rendowRow(obj);
-        }
-      });
-    }
-  }
-  
-  function updateSelectOptions() {
-    let options = [];
-    
-    todoList.forEach((obj)=>{
-      options.push(obj.category);
-    });
-    
-    let optionsSet = new Set(options);
-    
-    selectElem.innerHTML = "";
-    
-    let newOptionElem = document.createElement('option');
-    newOptionElem.value = DEFAULT_OPTION;
-    newOptionElem.innerText = DEFAULT_OPTION;
-    selectElem.appendChild(newOptionElem);
-    
-    for (let option of optionsSet) {
-      let newOptionElem = document.createElement('option');
-      newOptionElem.value = option;
-      newOptionElem.innerText = option;
-      selectElem.appendChild(newOptionElem);
-    }
-    
-    
   }
   
   function save() {
-    let stringified = JSON.stringify(todoList);
-    localStorage.setItem("todoList", stringified);
+    let stringified = JSON.stringify(bookList);
+    localStorage.setItem("bookList", stringified);
   }
   
   function load() {
-    let retrieved = localStorage.getItem("todoList");
-    todoList = JSON.parse(retrieved);
-    if (todoList == null)
-      todoList = [];
+    let retrieved = localStorage.getItem("bookList");
+    bookList = JSON.parse(retrieved);
+    if (bookList == null)
+      bookList = [];
   }
   
   function renderRows() {
-    todoList.forEach(todoObj => {
-      rendowRow(todoObj);
+    bookList.forEach(bookItem => {
+      renderRow(bookItem);
     })
   }
   
-  function rendowRow({todo: eventNameValue, category: categoryValue, id, dateStart, dateEnd, color}) {
-    
-    let table = document.getElementById("todoTable");
-    
+  function renderRow(obj) {
+
+    let table = document.getElementById("bookTable");
+
     let trElem = document.createElement("tr");
     table.appendChild(trElem);
-    
+
     let dateElem = document.createElement("td");
-    let dateObj = new Date(dateStart);
+    let dateObj = new Date(obj.dateStart);
     dateElem.innerText = dateObj.toLocaleString("en-GB", {
       month: "long",
       day: "numeric",
       year: "numeric",
     });
     trElem.appendChild(dateElem);
-    
+    let tdElem1 = document.createElement("td");
+    tdElem1.innerText = obj.client;
+    trElem.appendChild(tdElem1);
     let tdElem2 = document.createElement("td");
-    tdElem2.innerText = eventNameValue;
+    tdElem2.innerText = obj.place;
     trElem.appendChild(tdElem2);
     let tdElem3 = document.createElement("td");
-    tdElem3.innerText = categoryValue;
+    tdElem3.innerText = obj.field;
     tdElem3.className = "categoryCell";
     trElem.appendChild(tdElem3);
-    
+
     let spanElem = document.createElement("span");
     spanElem.innerText = "удалить";
     spanElem.className = "material-icons";
     spanElem.addEventListener("click", deleteItem, false);
-    spanElem.dataset.id = id;
+    spanElem.dataset.id = obj.id;
     let tdElem4 = document.createElement("td");
     tdElem4.appendChild(spanElem);
     trElem.appendChild(tdElem4);
-    
+
     addEvent({
-      id: id,
-      title: eventNameValue,
-      start: new Date(dateStart),
-      end: new Date(dateEnd),
-      color: color
+      id: obj.id,
+      title: obj.client,
+      start: new Date(obj.dateStart),
+      end: new Date(obj.dateEnd),
+      color: obj.color,
+      extendedProps: {
+        bookType: obj.bookType,
+        place: obj.place,
+        field: obj.field,
+        referee: obj.referee,
+        amountPaid: obj.amountPaid
+      }
     });
     
     function deleteItem() {
       trElem.remove();
-      updateSelectOptions();
       
-      for (let i = 0; i < todoList.length; i++) {
-        if (todoList[i].id === this.dataset.id)
-          todoList.splice(i, 1);
+      for (let i = 0; i < bookList.length; i++) {
+        if (bookList[i].id === this.dataset.id)
+          bookList.splice(i, 1);
       }
       save();
   
@@ -222,20 +210,31 @@ function todoMain() {
       },
       aspectRatio: 3,
       customButtons: {
-        myCustomButton: {
+        calendarFree: {
           text: 'Свободные часы',
           click: function() {
             calendarEl.classList.toggle("calendar-free");
+            calendar.setOption('eventsColor', '#378006');
+          }
+        },
+        changeInterval: {
+          text: 'Изменить интервал',
+          click: function() {
+            if (calendar.getOption('slotDuration') === '01:00:00') {
+              calendar.setOption('slotDuration', '00:10:00');
+            } else {
+              calendar.setOption('slotDuration', '01:00:00');
+            }
           }
         }
       },
       headerToolbar: {
-        left: 'prevYear,prev,next,nextYear today myCustomButton',
+        left: 'prevYear,prev,next,nextYear today calendarFree changeInterval',
         center: 'title',
         right: 'dayGridMonth,timeGridWeek,timeGridDay'
       },
       footerToolbar: {
-        left: 'prevYear,prev,next,nextYear today myCustomButton',
+        left: 'prevYear,prev,next,nextYear today calendarFree changeInterval',
         center: 'title',
         right: 'dayGridMonth,timeGridWeek,timeGridDay'
       },
@@ -263,21 +262,18 @@ function todoMain() {
           if (holidays[i]._d.getFullYear()+'-'+(holidays[i]._d.getMonth()+1)+'-'+holidays[i]._d.getDate() === info.start.getFullYear()+'-'+(info.start.getMonth()+1)+'-'+info.start.getDate()) {
             alert('Не рабочий день');
             return;
+          } else {
+            initForm(info)
           }
-          calendarForm.style.display = "flex";
-          dateStartInput.value = new Date(info.startStr).toISOString().slice(0, -8);
-          dateEndInput.value = new Date(info.endStr).toISOString().slice(0, -8);
-          formClose.addEventListener('click', function (event) {
-            event.preventDefault();
-            calendarForm.style.display="none";
-          })
         }
       },
       timeZone: 'UTC',
       events: [],
       locale: 'ru',
+      slotDuration: '01:00:00',
       eventClick: function(info) {
-        alert('Название события: ' + info.event.title+';');
+        console.log(info.event);
+        alert('Тип: ' + info.event.extendedProps.bookType+'; Клиент: ' + info.event.title+'; Судья: ' + info.event.extendedProps.referee+'; Сумма к оплате: ' + info.event.extendedProps.amountPaid+'; Оплачено: ' + info.event.extendedProps.amountPaid+'; Ссылка на карточку заказа: тут будет ссылка;');
         alert('Так же при клике можно вывести модальное окно c данными события');
       },
       datesSet: function (view) {
@@ -305,5 +301,24 @@ function todoMain() {
   function addEvent(event) {
     calendar.addEvent(event)
     calendarForm.style.display="none";
+  }
+
+  function initForm (info) {
+    calendarForm.style.display = "flex";
+    dateStartInput.value = new Date(info.startStr).toISOString().slice(0, -8);
+    dateEndInput.value = new Date(info.endStr).toISOString().slice(0, -8);
+    let start = Date.parse(dateStartInput.value);
+    let end = Date.parse(dateEndInput.value);
+    let totalHours = NaN;
+    if (start < end) {
+      totalHours = Math.floor((end - start) / 1000 / 60 / 60);
+    }
+
+    amountPaid.value = totalHours * COST;
+
+    formClose.addEventListener('click', function (event) {
+      event.preventDefault();
+      calendarForm.style.display="none";
+    })
   }
 }
